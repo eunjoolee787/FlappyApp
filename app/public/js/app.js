5211a4ab6d6f1f51fd55d1b74d7550b909f830aa
Eun Joo <eunjoolee787@gmail.com>
2014-12-03 21:49:42 -1000

    replace images

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -12,5 +12,16 @@ $(function() {
       $("p.lorem").html(data.body);
     });
   });
+
+  // $("span.bird").on("mouseenter", function(){
+  //   console.log("hi");
+  // });
+  $("p.lorem").on("mouseenter", "span.bird", function(){
+    $(this).html('<img src="/images/logo.png" alt="bird icon">');
+  });
+
+   $("p.lorem").on("mouseleave", "span.bird", function(){
+    $(this).html('bird');
+  });
 });
 
