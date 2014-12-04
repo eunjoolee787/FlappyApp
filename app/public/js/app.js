5a80867df4e417e79c01407253ca532353ef9772
Eun Joo <eunjoolee787@gmail.com>
2014-12-03 21:07:18 -1000

    add nav's specs link

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -1,6 +1,11 @@
 $(function() {
-  $.get( "/api/about.json", function( data ) {  
-     $("p.lorem").append(data.body);
 
-  }, "json" );
+
+  $("#specs_nav_link").on("click", function(){
+    $.get( "/api/about.json", function( data ) {  
+      $("p.lorem").html(data.body);
+    }); 
+  });
+
 });
+
