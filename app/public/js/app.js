ed7b4503762a31c2a726e2e90ff561e1a00944ef
Eun Joo <eunjoolee787@gmail.com>
2014-12-03 20:13:50 -1000

    download 2 json data files

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -1,5 +1,18 @@
 $(function() {
+  function (event) {
 
-  console.log("hello");
+    event.preventDefault();
+    $.ajax({
+      type: "GET",
+      url: "http://class.sandbox.devleague.com/data/flappy_app/about.json";
+      contentType: "application/json",
+      data: {
+        public/api/about.json
+      },
+      success: function(response){
+        
+      }
+    });
+  }
 
 });
